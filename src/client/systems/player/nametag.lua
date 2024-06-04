cdb8c7de9f25f11f841130010a203f084cf2c3be
triducal <thebingobangoyt@gmail.com>
2024-06-04 18:59:42 -0500

    New assets folder + nametag gui

--- src/client/systems/player/nametag.lua
+++ src/client/systems/player/nametag.lua
@@ -1,5 +1,6 @@
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterGui = game:GetService("StarterGui")
 local Player = Players.LocalPlayer
 local Components = require(ReplicatedStorage.Shared.components)
 local Matter = require(ReplicatedStorage.Packages.matter)
@@ -12,15 +13,10 @@ Adds a gui to any entity with a nametag that doesn't have one
 local function nametags(world: Matter.World)
 	for id, _, model, nametag in world:query(Components.Entity, Components.Model, Components.Nametag) do
 		if nametag.gui == nil then
-			local billboardGui = Instance.new("BillboardGui")
-			billboardGui.Size = UDim2.new(0, 200, 0, 50)
+			local billboardGui = StarterGui.Nametag:Clone()
 			billboardGui.Adornee = model.model
 			billboardGui.Parent = Player.PlayerGui
-
-			local textLabel = Instance.new("TextLabel")
-			textLabel.Size = UDim2.new(1, 0, 1, 0)
-			textLabel.Text = nametag.name
-			textLabel.Parent = billboardGui
+			billboardGui.TextLabel.Name = nametag.name
 
 			world:remove(id, Components.Nametag)
 			world:insert(
--- src/shared/components.lua
+++ src/shared/components.lua
@@ -3,9 +3,9 @@ local Matter = require(ReplicatedStorage.Packages.matter)
 
 return {
 	Model = Matter.component("Model") :: { model: Model },
-	Transform = Matter.component("Transform") :: { x: number, y: number, z: number, rx: number, ry: number, rz: number },
 	Health = Matter.component("Health") :: { health: number, maxHealth: number },
 	Player = Matter.component("Player") :: { name: string },
 	Nametag = Matter.component("Nametag") :: { name: string, gui: BillboardGui },
 	Entity = Matter.component("Entity"),
+	Block = Matter.component("Block") :: { type: string, active: boolean },
 }
